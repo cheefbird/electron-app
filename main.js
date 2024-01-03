66a8bc183ca2c556dc4946375ba035e3842826c1
Francis Breidenbach <fbreidenbach@gmail.com>
2024-01-03 13:35:44 -0800

    windows lifecycle

--- main.js
+++ main.js
@@ -9,6 +9,18 @@ const createWindow = () => {
     window.loadFile('index.html')
 }
 
-app.on('ready', () => {
+app.whenReady().then(() => {
     createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
